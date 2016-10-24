8412bd96c629fab3a8e2c5f5555044b67641da2c
colinbinion <colin.binion@me.com>
2016-10-24 08:32:31 -0600

    update books

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var databaseConnection = require("../database_connection");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  databaseConnection("book").select().then(function(book){
+    res.send(book);
+  });
 });
 
 module.exports = router;
